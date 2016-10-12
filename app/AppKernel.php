9e13a4f6443351cf0e444a592c90415b0bb36d27
maximebourdel <bourdel.maxime.pro@gmail.com>
2016-10-13 01:01:58 +0200

    MBO - Ajout REST API + Bazinga faker (tests) + Nelmio

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,12 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new MainBundle\MainBundle(),
+                
+            new FOS\RestBundle\FOSRestBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+                
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
@@ -24,6 +30,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Bazinga\Bundle\FakerBundle\BazingaFakerBundle();
         }
 
         return $bundles;
--- src/MainBundle/Entity/Book.php
+++ src/MainBundle/Entity/Book.php
@@ -187,4 +187,3 @@ class Book
         return $this->active;
     }
 }
-
